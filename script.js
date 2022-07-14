1fbdbf205fa3e6186defcbd1851c162582956233
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2022-07-14 18:48:13 -0300

    exercicios 9. 10. 11

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ const createCustomElement = (element, className, innerText) => {
 };
 
 const saveCartItemsComplet = (item) => {
-    const locaStorageElement = localStorage.getItem('cartItems');
-    if (locaStorageElement === null) {
-      localStorage.setItem('cartItems', JSON.stringify([item]));
-    } else {
-      const objectsLocalStorage = JSON.parse(locaStorageElement);
-      localStorage.setItem('cartItems', JSON.stringify([...objectsLocalStorage, item]));
-    }
+  const locaStorageElement = localStorage.getItem('cartItems');
+  if (locaStorageElement === null) {
+    localStorage.setItem('cartItems', JSON.stringify([item]));
+  } else {
+    const objectsLocalStorage = JSON.parse(locaStorageElement);
+    localStorage.setItem('cartItems', JSON.stringify([...objectsLocalStorage, item]));
+  }
 };
 
 const container = document.querySelector('#cart_item');
@@ -34,27 +34,18 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
-const cartItemClickListener = (event) => {
-  const item = event.target;
-  console.log(item);
-  container.removeChild(item);
-  // arrLi.splice(arrLi.indexOf(item));
-  // saveCartItems(arrLi);
+const priceSubtrai = (element) => {
+  const parceResult = JSON.parse(localStorage.getItem('totalPrice'));
+  const subResult = parceResult - element;
+  localStorage.setItem('totalPrice', subResult);
+  document.querySelector('#price').innerHTML = subResult;
 };
 
-// const createArr = (param) => {
-//   const itemAddList = param.innerHTML;
-//   arrLi.push(`${itemAddList};`);
-// };
-
-const createCartItemElement = ({ sku, name, salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  // createArr(li);
-  // saveCartItems(arrLi);
-  return li;
+const cartItemClickListener = (event) => {
+  const item = event.target;
+  const itemValue = item.innerText.split('$')[1];
+  container.removeChild(item);
+  priceSubtrai(itemValue);
 };
 
 const objArray = async (element) => {
@@ -83,11 +74,40 @@ const createListFunct = async (element) => {
 const section = document.getElementsByClassName('items');
 const button = section[0].childNodes;
 
+const priceSoma = (element) => {
+  let total = 0;
+  let resercheNumber = 0;
+  if (localStorage.length === 0) {
+    total = 0;
+  } else {
+    total = localStorage.getItem('totalPrice');
+    resercheNumber += JSON.parse(total);
+  }
+  const result = resercheNumber + element;
+  localStorage.setItem('totalPrice', result);
+};
+
+const totalPrice = (element) => {
+  const priceLocal = document.querySelector('#price');
+  // const intNumber = parseInt(element);
+  priceLocal.innerHTML = element;
+  return priceLocal;
+};
+
+const createCartItemElement = ({ sku, name, salePrice }) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  const totalPriceResurge = localStorage.getItem('totalPrice');
+  totalPrice(totalPriceResurge);
+  return li;
+};
+
 const addIntemInCart = () => {
   button.forEach((element) => {
     const elemento = element.lastChild;
     elemento.addEventListener('click', async function (e) {
-      // const container = document.querySelector('#cart_item');
       const selectedItem = e.path[1].firstChild.innerText;
       const objSelect = await fetchItem(selectedItem);
       const objReturn = {
@@ -96,15 +116,48 @@ const addIntemInCart = () => {
         salePrice: objSelect.price,
       };
       saveCartItemsComplet(objReturn);
+      const objPrice = objReturn.salePrice;
+      priceSoma(objPrice);
       container.appendChild(createCartItemElement(objReturn));
       return container;
     });
   });
 };
 
+const emptyButton = document.querySelector('.empty-cart');
+
+emptyButton.addEventListener('click', () => {
+  localStorage.clear();
+  const arrLi = document.getElementsByClassName('cart__items')[0].children;
+  const arrLi2 = [...arrLi];
+  console.log(arrLi);
+  arrLi2.forEach((element) => {
+    element.remove();
+  });
+  totalPrice(0);
+});
+
+const hideLoading = () => {
+  const load = document.querySelector('.loading');
+  load.remove();
+};
+
+const showloading = () => {
+  console.log('ok');
+  const div = document.createElement('div');
+  div.classList.add('loading');
+  document.body.appendChild(div);
+  const label = document.createElement('label');
+  label.innerText = 'Carregando...';
+  div.appendChild(label);
+};
+
 window.onload = async () => {
+  showloading();
   await createListFunct('computador');
+  hideLoading();
   addIntemInCart();
+  document.querySelector('#price').innerHTML = localStorage.getItem('totalPrice');
   const itensInStorage = getSavedCartItems('cartItems');
   const objParam = JSON.parse(itensInStorage);
   if (localStorage.length === 0) {
@@ -113,5 +166,4 @@ window.onload = async () => {
     objParam.forEach((element) => container.appendChild(createCartItemElement(element)));
     console.log('ola');
   }
-  console.log(objParam);
 };
