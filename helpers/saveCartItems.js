e8667c6871a0ce87f3a9def793c7c094b7fb4779
Bruno de Oliveira <historia.brunooliveira1993@gmail.com>
2023-03-21 16:34:09 -0300

    finalizando prejeto

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,7 +1,5 @@
 const saveCartItem = (item) => localStorage.setItem('cartItems', item);
 
-// console.log(saveCartItemsComplet());
-
 if (typeof module !== 'undefined') {
   module.exports = saveCartItem;
 }
